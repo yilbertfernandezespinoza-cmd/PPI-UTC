b5bc586d8f7c47e4023b7d2d5e85257265638cb0
yilbertfernandezespinoza-cmd <yilbertfenandez.espinoza@gmail.com>
2026-07-01 22:27:55 -0600

    creacion de base de datos

---
+++ SIGEPAN/database/ddl/01_create_database.sql
@@ -0,0 +1,13 @@
+-- ==========================================================
+-- PROYECTO: SIGEPAN
+-- Archivo: 01_create_database.sql
+-- Descripción: Creación de la Base de Datos
+-- ==========================================================
+
+DROP DATABASE IF EXISTS sigepan_db;
+
+CREATE DATABASE sigepan_db
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_spanish_ci;
+
+USE sigepan_db;

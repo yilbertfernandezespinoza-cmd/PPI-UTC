b2e2632cb8c755b39616d3a6ca7dbc5d1a4ab1ae
Cacttermus <cacttermus@gmail.com>
2026-07-14 11:13:57 -0600

    Avance 3
    integraciones de proveedores completada

--- SIGEPAN/backend/apps/proveedores/forms.py
+++ SIGEPAN/backend/apps/proveedores/forms.py
@@ -1,1 +1,16 @@
 # Formularios del módulo
+from django import forms
+from .models import Proveedor
+
+class ProveedorForm(forms.ModelForm):
+    class Meta:
+        model = Proveedor
+        fields = ['identificacion', 'nombre', 'contacto', 'telefono', 'correo', 'direccion', 'estado']
+        labels = {
+            'identificacion': 'Identificación',
+            'nombre': 'Nombre',
+            'contacto': 'Contacto',
+            'telefono': 'Teléfono',
+            'correo': 'Correo electrónico',
+            'direccion': 'Dirección',
+        }
